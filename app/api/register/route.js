612c1adb2eb31b6e57030b18c0fbf8cd489753e2
Hamid Ghanbari <70950300+hghanbari@users.noreply.github.com>
2025-11-08 00:43:50 +0100

    last test

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
+if (!process.env.RESEND_API_KEY) {
+  throw new Error("کلید RESEND_API_KEY پیدا نشد!");
+}
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req) {
   const body = await req.json();
